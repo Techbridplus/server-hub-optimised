4eb0d7f4f1083fc9c408db2eb2b927976a383aed
Gaurav Joshi <gauravjoshiaa1@gmail.com>
2025-06-07 23:16:34 +0530

    trying

--- src/lib/socket-client.ts
+++ src/lib/socket-client.ts
@@ -4,7 +4,7 @@ let socket: Socket | null = null
 
 export const initSocket = (userId?: string, serverId?: string) => {
   if (!socket) {
-    socket = io(process.env.NEXT_PUBLIC_SOCKET_URL || "https://server-hub-socket-server.onrender.com", {
+    socket = io( "https://server-hub-socket-server.onrender.com", {
       query: {
         userId,
         serverId
