439497102d8996cd2f0cd9201e35922c8bd8a847
Saurabh Chandra Baij <saurabhbaij@gmail.com>
2025-06-30 18:48:54 +0530

    Added Vercel Analytics for analytics purposes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google"
 import "./globals.css";
 import { Providers } from "@/app/providers"
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         className={`${inter.className} antialiased`}
       >
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   );
